86be98b867f965f77314ce90b0e51bafc8b92e06
timeforkevin <kevinjin@live.ca>
2016-02-08 22:48:10 -0500

    First Commit for Test App

---
+++ src/components/ComponentBase.java
@@ -0,0 +1,11 @@
+package components;
+
+public class ComponentBase {
+
+	protected int num;
+	protected String name;
+	
+	public ComponentBase() {
+		
+	}
+}
---
+++ src/controller/ComponentActual.java
@@ -0,0 +1,11 @@
+package controller;
+
+import components.ComponentBase;
+
+public class ComponentActual extends ComponentBase {
+
+	public ComponentActual() {
+		this.name = "ComponentActual";
+		this.num = 1;
+	}
+}
